46d2ab5a969602e2f44e9aac0041295ff4a331a4
ysally77 <80692304+ysally77@users.noreply.github.com>
2024-05-03 13:35:50 +0900

    Update CameraControl.cs

--- Camera/CameraControl.cs
+++ Camera/CameraControl.cs
@@ -28,3 +28,4 @@ public class CameraControl : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
+// https://with-rl.com/URDF%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EB%A7%8C%EB%93%A0-%EB%A1%9C%EB%B4%87%EC%97%90-Unity-%EC%9E%90%EB%8F%99%EC%B0%A8-%EC%97%B0%EA%B2%B0%ED%95%98%EA%B8%B0-2/ 참고
